2cd2c19b33918519b4cd399f3f19a8ee2053e4cc
Mikael Pro <teddi201252@gmail.com>
2021-07-28 23:32:33 +0200

    Dropped view engine idea, return to monkey

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const app = express();
 const pg = require('pg');
 const PORT = process.env.PORT || 5000;
 
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-
 const { Pool } = require('pg');
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -14,15 +11,18 @@ const db = new Pool({
   }
 });
 
-
 app.listen(PORT, () => console.log('Connected'));
+
+app.get('/', function (req, res) {
+  res.send('Hello there');
+});
 
 app.get('/prova', async (req, res) => {
   try {
     const client = await db.connect();
     const result = await client.query('SELECT * FROM test_table');
     const results = { 'results': (result) ? result.rows : null};
-    res.render('index', results);
+    res.send(results);
     client.release();
   } catch (err) {
     console.error(err);
