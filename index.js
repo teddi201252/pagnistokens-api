60e52bcd30559acd9d63adeb577c5ecfb7b08b53
Mikael Pro <teddi201252@gmail.com>
2021-07-28 16:51:28 +0200

    Safe postgres URI

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const pg = require('pg');
 const PORT = process.env.PORT || 5000;
 
-const connectionPG = "postgres://uztocliqrifgod:9e12c387cd75a910b5471e414a394c0766a2cc188dc2c9ba7459e0023a727291@ec2-52-6-211-59.compute-1.amazonaws.com:5432/d9i98rskf7476v"
+const connectionPG = process.env.DATABASE_URL;
 const pgClient = new pg.Client(connectionPG);
 pgClient.connect();
 
