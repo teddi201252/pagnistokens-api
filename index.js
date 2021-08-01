b8db14acc48fa2902e02e210e0fbc7ab77420992
Mikael Pro <teddi201252@gmail.com>
2021-08-01 20:22:18 +0200

    Added more api calls

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ const mysql = require('mysql2');
 const PORT = process.env.PORT;
 
 const welcomePage = '/pagnistokens/';
-const usersUnavailablePage = '/pagnistokens/users';
-const usersPage = '/pagnistokens/users/:id';
+const usersPage = '/pagnistokens/users';
+const getUserPage = '/pagnistokens/users/:id';
 const getWalletPage = '/pagnistokens/wallets/:id';
 const getUserByWalletPage = '/pagnistokens/users/walletid/:walletId';
+const notificationsPage = '/pagnistokens/notifications';
+const friendshipIsMagicPage = '/pagnistokens/friends';
+
+/*
+ERRORS LIST
+
+466 = bad json parsing
+420 = bad query
+
+*/
 
 const dbConfig = {
   host: 'localhost',
@@ -25,19 +35,21 @@ app.get(welcomePage, function (req, res) {
   res.send('Hello there');
 });
 
-app.get(usersUnavailablePage, function (req, res) {
+app.get(usersPage, function (req, res) {
   const connection = mysql.createConnection(dbConfig);
   connection.connect();
   connection.query('SELECT * from Users', function(err, rows, fields){
     if(err){
-      res.send(err);
+      res.status(420).send(err);
     } 
-    res.send(rows);
+    else{
+      res.send(rows);
+    }
   });
   connection.end();
 });
 
-app.get(usersPage, (req, res) => {
+app.get(getUserPage, (req, res) => {
   try {
     const { id } = req.params;
     const values = [ id ];
@@ -45,11 +57,13 @@ app.get(usersPage, (req, res) => {
     connection.connect();
     connection.query('SELECT username, walletid from Users WHERE id = ?', values, function(err, rows, fields){
       if(err){
-        res.send(err);
+        res.status(420).send(err);
       } 
-      res.send(rows[0]);
+      else{
+        res.send(rows[0]);
+      }
     });
-  connection.end();
+    connection.end();
   } catch (err) {
     res.send("Error " + err);
   }
@@ -62,9 +76,12 @@ app.get(getWalletPage, (req, res) => {
   connection.connect();
   connection.query('SELECT * from Wallets WHERE id = ?', values, function(err, rows, fields){
     if(err){
-      res.send(err);
+      res.status(420).send(err);
     } 
-    res.send(rows[0]);
+    else {
+      res.send(rows[0]);
+    }
+   
   });
   connection.end();
 });
@@ -76,9 +93,92 @@ app.get(getUserByWalletPage, (req, res) => {
   connection.connect();
   connection.query('SELECT * from Users WHERE walletid = ?', values, function(err, rows, fields){
     if(err){
-      res.send(err);
+      res.status(420).send(err);
     } 
-    res.send(rows[0]);
+    else{
+      res.send(rows[0]);
+    }
   });
   connection.end();
 });
+
+app.post(notificationsPage, (req, res) => {
+  try {
+    const {idUser, title, message} = req.body;
+    const values = [idUser, title, message];
+    const connection = mysql.createConnection(dbConfig);
+    connection.connect();
+    connection.query('INSERT INTO Notifications (idUser, title, message) VALUES (?, ?, ?)', values, (err, rows, fields) => {
+      if(err){
+        res.status(420).send(err);
+      }
+      else{
+        res.send(rows);
+      }
+    });
+    connection.end();
+  }
+  catch(err){
+    res.status(466).send("Something's wrooong i can feel it " + err)
+  }
+});
+
+app.route(friendshipIsMagicPage)
+  .post((req, res) => {
+    try {
+      const { id1, id2 } = req.body;
+      const values = [id1, id2];
+      const connection = mysql.createConnection(dbConfig);
+      connection.connect();
+      connection.query('INSERT INTO FriendRelations (id1, id2) VALUES (?, ?)', values, (err, rows, fields) => {
+        if(err){
+          res.status(420).send(err);
+        }
+        else{
+          res.send(rows);
+        }
+      });
+      connection.end();
+    } 
+    catch(err){
+      res.status(466).send("Something's wrooong i can feel it " + err)
+    }
+  })
+  .put((req, res) => {
+    try {
+      const {id1, id2} = req.body;
+      const values = [id1, id2];
+      const connection = mysql.createConnection(dbConfig);
+      connection.connect();
+      connection.query('UPDATE FriendRelations SET accepted = true WHERE id1 = ? AND id2 = ?', values, (err, rows, fields) => {
+        if(err){
+          res.status(420).send(err);
+        }
+        else{
+          res.send(rows);
+        }
+      });
+      connection.end();
+    }catch(err){
+      res.status(466).send("Something's wrooong i can feel it " + err)
+    }
+  })
+  .delete((req, res) => {
+    try {
+      const {id1, id2} = req.body;
+      const values = [id1, id2];
+      const connection = mysql.createConnection(dbConfig);
+      connection.connect();
+      connection.query('DELETE FROM FriendRelations WHERE id1 = ? AND id2 = ?', values, (err, rows, fields) => {
+        if(err){
+          res.status(420).send(err);
+        }
+        else{
+          res.send(rows);
+        }
+      });
+      connection.end();
+    }catch(err){
+      res.status(466).send("Something's wrooong i can feel it " + err)
+    }
+  });
